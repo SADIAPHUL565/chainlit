22e740d02baa32cb429e4259019fb3b1c12f163b
sadiaphul <sadiabaloch565@gmail.com>
2025-07-31 17:24:54 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os, chainlit as cl
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent, Runner, OpenAIChatCompletionsModel
+
+load_dotenv()
+
+client= AsyncOpenAI(
+    api_key=os.getenv("GEEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+
+@cl.on_message
+
+async def main(message: cl.Message):
+    agent = Agent(
+        name="Assistant",
+        instructions="You are a helpful Assistant",
+        model=OpenAIChatCompletionsModel(
+            model="gemini-2.0-flash",
+            openai_client=client
+        )
+    )
+    result = await Runner.run(agent,message.content)
+    
+    await cl.Message(content=result.final_output).send()
